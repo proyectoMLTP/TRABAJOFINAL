d4f65918474c06848a51bdd8d410fe7527c760e6
ElvinnKemmis <elvin11_99@hotmail.com>
2019-06-16 14:25:31 -0500

    creacion de tabla alertas en la base de datos

---
+++ Migrations/20190616192436_alertas.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TRABAJOFINAL.Migrations
+{
+    public partial class alertas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Alertas",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("MySql:ValueGenerationStrategy", MySqlValueGenerationStrategy.IdentityColumn),
+                    nombre = table.Column<string>(nullable: true),
+                    telefono = table.Column<string>(nullable: true),
+                    direccion = table.Column<string>(nullable: true),
+                    descripcion = table.Column<string>(nullable: true),
+                    usuarioId = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Alertas", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Alertas_AspNetUsers_usuarioId",
+                        column: x => x.usuarioId,
+                        principalTable: "AspNetUsers",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Alertas_usuarioId",
+                table: "Alertas",
+                column: "usuarioId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Alertas");
+        }
+    }
+}
