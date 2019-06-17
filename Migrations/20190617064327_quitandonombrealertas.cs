609aafe4c9767667e8a5134410be06d230bb5580
ElvinnKemmis <elvin11_99@hotmail.com>
2019-06-17 01:44:58 -0500

    alertas sin nombre usuario

---
+++ Migrations/20190617064327_quitandonombrealertas.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace TRABAJOFINAL.Migrations
+{
+    public partial class quitandonombrealertas : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "nombre",
+                table: "Alertas");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "nombre",
+                table: "Alertas",
+                nullable: true);
+        }
+    }
+}
--- Models/Alerta.cs
+++ Models/Alerta.cs
@@ -3,8 +3,6 @@ namespace TRABAJOFINAL.Models
     public class Alerta
     {
         public int Id { get; set; }
-        public string  nombre { get; set; }
-
         public string telefono { get; set; }
         public string direccion { get; set; }
 
